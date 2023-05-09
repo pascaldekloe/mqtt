e0ce552bc77d2c6c24a32756ccf1366e42fd1f51
Pascal S. de Kloe <pascal@quies.net>
2023-05-09 17:15:47 +0200

    integration test overhaul, tests multiple in-flight mesages

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -1,23 +1,16 @@
 package integration
 
 import (
-	"context"
 	"encoding/binary"
 	"errors"
 	"net"
 	"os"
 	"strings"
-	"sync"
 	"testing"
 	"time"
 
 	"github.com/pascaldekloe/mqtt"
 )
-
-// BatchSize is a reasonable number of messages which should not cause any of
-// them to be dropped (by the broker) when send sequentially.
-const batchSize = 99
-const batchTimeout = time.Minute
 
 func hosts(tb testing.TB) []string {
 	s, ok := os.LookupEnv("MQTT_HOSTS")
@@ -27,155 +20,79 @@ func hosts(tb testing.TB) []string {
 	return strings.Fields(s)
 }
 
-// NewTestClient returns an instance for testing.
-func newTestClient(t *testing.T, host string, config *mqtt.Config) (client *mqtt.Client, messages <-chan uint64) {
-	config.Dialer = mqtt.NewDialer("tcp", net.JoinHostPort(host, "1883"))
-	config.PauseTimeout = 2 * time.Second
-	config.CleanSession = true
-	client, err := mqtt.VolatileSession(t.Name(), config)
-	if err != nil {
-		t.Fatal("volatile session error:", err)
-	}
+// BatchSize is a reasonable number of messages which should not cause any of
+// them to be dropped (by the broker) when send sequentially.
+const batchSize = 128
 
-	// messages contain their respective sequence number
-	ch := make(chan uint64, 16)
-	t.Cleanup(func() {
-		err := client.Close()
-		if err != nil {
-			t.Error("client close error:", err)
-		}
-		// await read-routine exit
-		seqNo, ok := <-ch
-		if ok {
-			t.Errorf("got message # %d after close", seqNo)
-		}
-	})
-	// launch read routine
-	go func() {
-		defer close(ch)
-		for {
-			message, topic, err := client.ReadSlices()
-			switch {
-			case err == nil:
-				if len(message) != 8 {
-					t.Errorf("unexpected message %#x on topic %q", message, topic)
-				} else {
-					ch <- binary.LittleEndian.Uint64(message)
-				}
+// SendBatch publishes a total of batchSize messages, with 8-byte payloads,
+// containing msgOffset until msgOffset + batchSize − 1. It pushes for the
+// maximum number of in-flight messages allowed by the mqtt.Client.
+func sendBatch(t testing.TB, publish func([]byte, string) (<-chan error, error), msgOffset uint64) {
+	retry := time.NewTicker(time.Microsecond)
+	defer retry.Stop()
 
-			case errors.Is(err, mqtt.ErrClosed):
-				return
+	// confirmation channels
+	var exchanges [batchSize]<-chan error
+	topic := t.Name()
 
-			default:
-				t.Log(err)
-				time.Sleep(time.Second / 2)
-			}
-		}
-	}()
-
-	for {
-		err := client.Subscribe(nil, t.Name())
+	// publish each
+	for i := uint64(0); i < batchSize; {
+		var msg [8]byte
+		binary.BigEndian.PutUint64(msg[:], msgOffset+i)
+		ch, err := publish(msg[:], topic)
 		switch {
 		case err == nil:
-			return client, ch
-		case errors.Is(err, mqtt.ErrDown):
-			time.Sleep(10 * time.Millisecond)
-			continue
+			exchanges[i] = ch
+			i++
+
+		case errors.Is(err, mqtt.ErrMax):
+			<-retry.C
+
 		default:
-			t.Fatal(err)
+			t.Error("publish batch abort on:", err)
+			return
+		}
+	}
+
+	// read confirmations
+	for i := range exchanges {
+		for err := range exchanges[i] {
+			t.Error("publish exchange abort on:", err)
+			return
 		}
 	}
 }
 
-func TestRace(t *testing.T) {
-	for _, host := range hosts(t) {
-		t.Run(host, func(t *testing.T) {
-			t.Run("at-most-once", func(t *testing.T) {
-				client, messages := newTestClient(t, host, new(mqtt.Config))
-				raceAtLevel(t, client, messages, 0)
-			})
-			t.Run("at-least-once", func(t *testing.T) {
-				client, messages := newTestClient(t, host, &mqtt.Config{
-					AtLeastOnceMax: 9,
-				})
-				raceAtLevel(t, client, messages, 1)
-			})
-			t.Run("exactly-once", func(t *testing.T) {
-				client, messages := newTestClient(t, host, &mqtt.Config{
-					ExactlyOnceMax: 9,
-				})
-				raceAtLevel(t, client, messages, 2)
-			})
-		})
+// ReceiveBatch reads a sendBatch sequence.
+func receiveBatch(t testing.TB, messages <-chan uint64, offset uint64) {
+	for i := uint64(0); i < batchSize; i++ {
+		got, ok := <-messages
+			want := offset + i
+			if !ok {
+				t.Errorf("receive stopped, want message # %d", want)
+				return
+			}
+			if got != want {
+				t.Errorf("got message # %d, want # %d", got, want)
+			}
 	}
 }
 
-func raceAtLevel(t *testing.T, client *mqtt.Client, messages <-chan uint64, deliveryLevel int) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-	defer cancel()
-	testTopic := t.Name()
-
-	launch := make(chan struct{})
-
-	// install contenders
-	var wg sync.WaitGroup
-	defer wg.Wait()
-	wg.Add(batchSize)
-	for n := uint64(1); n <= batchSize; n++ {
-		go func(seqNo uint64) {
-			defer wg.Done()
-
-			var message [8]byte
-			binary.LittleEndian.PutUint64(message[:], seqNo)
-
-			<-launch
-
-			var exchange <-chan error
-		Publish:
-			for {
-				var err error
-				switch deliveryLevel {
-				case 0:
-					err = client.Publish(ctx.Done(), message[:], testTopic)
-				case 1:
-					exchange, err = client.PublishAtLeastOnce(message[:], testTopic)
-				case 2:
-					exchange, err = client.PublishExactlyOnce(message[:], testTopic)
-				}
-				switch {
-				case err == nil:
-					break Publish
-				case errors.Is(err, mqtt.ErrMax):
-					time.Sleep(200 * time.Microsecond)
-				case errors.Is(err, mqtt.ErrClosed):
-					return
-				default:
-					t.Errorf("publish #%d error: %s", seqNo, err)
-					return
-				}
-			}
-
-			if deliveryLevel != 0 {
-				err, ok := <-exchange
-				if ok {
-					t.Errorf("publish # %d exchange error: %s", seqNo, err)
-				}
-			}
-		}(n)
+func exchangeN(t testing.TB, n uint64, publish func([]byte, string) (<-chan error, error), messages <-chan uint64) {
+	if n%batchSize != 0 {
+		t.Fatalf("exchange count %d must be a multiple of %d", n, batchSize)
 	}
 
-	time.Sleep(50 * time.Millisecond)
-	close(launch)
-
-	timeout := time.After(batchTimeout)
-	for i := 0; i < batchSize; i++ {
-		select {
-		case _, ok := <-messages:
-			if !ok {
-				t.Fatalf("want %d more messages", batchSize-i)
-			}
-		case <-timeout:
-			t.Fatalf("timeout; want %d more messages", batchSize-i)
+	for i := uint64(0); i < n; i += batchSize {
+		done := make(chan struct{})
+		go func() {
+			defer close(done)
+			sendBatch(t, publish, i)
+		}()
+		receiveBatch(t, messages, i)
+		<-done
+		if t.Failed() {
+			return
 		}
 	}
 }
@@ -183,79 +100,98 @@ func raceAtLevel(t *testing.T, client *mqtt.Client, messages <-chan uint64, deli
 func TestRoundtrip(t *testing.T) {
 	for _, host := range hosts(t) {
 		t.Run(host, func(t *testing.T) {
-			const testN = 17_000 // causes an mqtt.publishIDMask overflow
-			t.Run("at-least-once", func(t *testing.T) {
-				client, messages := newTestClient(t, host, &mqtt.Config{
-					AtLeastOnceMax: 9,
-				})
-				for i := 0; i < testN; i += batchSize {
-					testRoundtripBatch(t, client, client.PublishAtLeastOnce, messages)
-				}
-			})
-
-			t.Run("exactly-once", func(t *testing.T) {
-				client, messages := newTestClient(t, host, &mqtt.Config{
-					ExactlyOnceMax: 9,
-				})
-				for i := 0; i < testN; i += batchSize {
-					testRoundtripBatch(t, client, client.PublishExactlyOnce, messages)
-				}
-			})
+			testRoundtrip(t, host)
 		})
 	}
 }
 
-func testRoundtripBatch(t *testing.T, client *mqtt.Client,
-	publish func(message []byte, topic string) (exchange <-chan error, err error),
-	messages <-chan uint64) {
-	testTopic := t.Name()
+func testRoundtrip(t *testing.T, host string) {
+	const messageN = 16384 + batchSize // overflows mqtt.publishIDMask
 
-	var wg sync.WaitGroup
-	defer wg.Wait()
+	// client instantiation
+	config := mqtt.Config{
+		Dialer:         mqtt.NewDialer("tcp", net.JoinHostPort(host, "1883")),
+		PauseTimeout:   time.Second,
+		CleanSession:   true,
+		AtLeastOnceMax: 9,
+		ExactlyOnceMax: 9,
+	}
+	client, err := mqtt.VolatileSession(t.Name(), &config)
+	if err != nil {
+		t.Fatal("client instantiation:", err)
+	}
 
-	wg.Add(1)
+	// read routine
+	atMostOnceMessages := make(chan uint64)
+	atLeastOnceMessages := make(chan uint64)
+	exactlyOnceMessages := make(chan uint64)
 	go func() {
-		defer wg.Done()
+		for {
+			defer close(atMostOnceMessages)
+			defer close(atLeastOnceMessages)
+			defer close(exactlyOnceMessages)
 
-		message := make([]byte, 8)
-		for n := uint64(1); n <= batchSize; {
-			binary.LittleEndian.PutUint64(message, n)
-			exchange, err := publish(message, testTopic)
-			switch {
-			case err == nil:
-				n++
+			message, topic, err := client.ReadSlices()
+			if err != nil {
+				t.Log(err)
+				if errors.Is(err, mqtt.ErrClosed) {
+					return
+				}
+				time.Sleep(time.Second / 2)
+				continue
+			}
 
-				wg.Add(1)
-				go func(seqNo uint64, exchange <-chan error) {
-					defer wg.Done()
-					err, ok := <-exchange
-					if ok {
-						t.Errorf("publish # %d exchange error: %s", seqNo, err)
-					}
-				}(n, exchange)
-			case errors.Is(err, mqtt.ErrMax):
-				time.Sleep(time.Millisecond)
-			case errors.Is(err, mqtt.ErrClosed):
-				return
+			if len(message) != 8 {
+				t.Errorf("unexpected message %#x on topic %q", message, topic)
+				continue
+			}
+			seqNo := binary.BigEndian.Uint64(message)
+
+			switch s := string(topic); {
+			case strings.HasSuffix(s, "/at-most-once"):
+				atMostOnceMessages <- seqNo
+			case strings.HasSuffix(s, "/at-least-once"):
+				atLeastOnceMessages <- seqNo
+			case strings.HasSuffix(s, "/exactly-once"):
+				exactlyOnceMessages <- seqNo
 			default:
-				t.Errorf("publish # %d error: %s", n, err)
-				return
+				t.Errorf("message # %d on unexpected topic %q", seqNo, topic)
 			}
 		}
 	}()
 
-	timeout := time.After(batchTimeout)
-	for n := 1; n <= batchSize; n++ {
-		select {
-		case seqNo, ok := <-messages:
-			if !ok {
-				t.Fatalf("did not receive message # %d–%d", n, batchSize)
-			}
-			if seqNo != uint64(n) {
-				t.Errorf("want message # %d, got # %d", n, seqNo)
-			}
-		case <-timeout:
-			t.Fatalf("timeout before message # %d-%d", n, batchSize)
+	<-client.Online()
+	t.Log("client online")
+
+	t.Run("at-most-once", func(t *testing.T) {
+		t.Parallel()
+		err := client.Subscribe(nil, t.Name())
+		if err != nil {
+			t.Fatal(err)
 		}
-	}
+		exchange := make(chan error)
+		close(exchange)
+		exchangeN(t, messageN, func(message []byte, topic string) (<-chan error, error) {
+			err := client.Publish(nil, message, topic)
+			return exchange, err
+		}, atMostOnceMessages)
+	})
+
+	t.Run("at-least-once", func(t *testing.T) {
+		t.Parallel()
+		err := client.Subscribe(nil, t.Name())
+		if err != nil {
+			t.Fatal(err)
+		}
+		exchangeN(t, messageN, client.PublishAtLeastOnce, atLeastOnceMessages)
+	})
+
+	t.Run("exactly-once", func(t *testing.T) {
+		t.Parallel()
+		err := client.Subscribe(nil, t.Name())
+		if err != nil {
+			t.Fatal(err)
+		}
+		exchangeN(t, messageN, client.PublishExactlyOnce, exactlyOnceMessages)
+	})
 }
